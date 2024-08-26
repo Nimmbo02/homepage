eb64e9a2d3390dd0adf604e3a57d6faf2e974019
nemr <nemraslam51@gmail.com>
2024-08-26 16:54:28 +0400

    title with scss

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,37 @@
-
 $(document).ready(function(){
     // Initialize the first carousel
-    $("#carousel1").owlCarousel({
+    $("#title-carousel").owlCarousel({
         loop: true,
         margin: 10,
         nav: true,
         rtl: true,
+        autoplay: true, 
+        autoplayTimeout: 3000, 
+        autoplayHoverPause: true,
         responsive: {
             0: {
-                items: 1
+                items: 1 // Show 1 item on small screens
             },
             600: {
-                items: 1
+                items: 1 // Show 1 item on medium screens
             },
             1000: {
-                items: 1
+                items: 1 // Show 1 item on large screens
             }
         }
     });
+
     // Initialize the second carousel
-    $("#carousel2").owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        rtl: true,
+    $("#ticker-carousel").owlCarousel({
+        loop: true, 
+        margin: 10, 
+        nav: true, 
+        rtl: true, 
+        autoplay: true, 
+        autoplayTimeout: 3000, 
+        autoplayHoverPause: true,
+        smartSpeed: 800,
+        autoplaySpeed: 1000, 
         responsive: {
             0: {
                 items: 2
