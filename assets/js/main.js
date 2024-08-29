01c9b4e88a52715fc6bd2ae5e12a1e78c8d343c4
nemr <nemraslam51@gmail.com>
2024-08-29 17:24:37 +0400

    corrected 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
         autoplaySpeed: 1000, 
         responsive: {
             0: {
-                items: 2
+                items: 1
             },
             600: {
                 items: 3
