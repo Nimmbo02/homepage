f799f4c4746d1c50c366cd7fd1398123ec92debd
nemr <nemraslam51@gmail.com>
2024-09-09 10:54:26 +0400

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,13 +10,7 @@ $(document).ready(function(){
         autoplayHoverPause: true,
         responsive: {
             0: {
-                items: 1 // Show 1 item on small screens
-            },
-            600: {
-                items: 1 // Show 1 item on medium screens
-            },
-            1000: {
-                items: 1 // Show 1 item on large screens
+                items: 1 
             }
         }
     });
@@ -37,9 +31,15 @@ $(document).ready(function(){
                 items: 1
             },
             600: {
-                items: 3
+                items: 2
             },
             1000: {
+                items: 3
+            },            
+            1300: {
+                items: 4
+            },
+            1400: {
                 items: 5
             }
         }
